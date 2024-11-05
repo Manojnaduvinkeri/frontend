62b50b13cb2897d34ed881bcf31d8c1197588ac0
Manojnaduvinkeri <naduvinkeri777@gmail.com>
2024-11-05 18:11:32 +0530

    Update Layout.jsx

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,22 +1,24 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
+
 export default function Layout() {
-	return (
-		<>
-			<nav>
-                <ul>
-                    <li>
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link to="/login">Login</Link>
-                    </li>
-                    <li>
-                        <Link to="/register">Register</Link>
-                    </li>
-                </ul>
-            </nav>
-            <Outlet/>
-		</>
-	);
+  return (
+    <>
+      <header>
+        <h1>Role-Based Authentication System (RBAC)</h1>
+        <p>Welcome to the RBAC system. This system provides access control based on user roles, allowing administrators and users to have different access levels.</p>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/register">Register</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </>
+  );
 }
