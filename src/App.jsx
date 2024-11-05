f6e2c0ec27c7dfe1a8b0fb165724d750067bb7aa
Manojnaduvinkeri <naduvinkeri777@gmail.com>
2024-11-05 18:12:29 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,23 @@
-import './App.css'
 import React from "react";
-import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
-import Layout from './pages/Layout';
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Dashboard from './pages/dashboard';
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./pages/layout";
+import Login from "./pages/login";
+import Register from "./pages/register";
+import Dashboard from "./pages/dashboard";
 
+function App() {
   return (
-   <HashRouter>
-   <Routes>
-    <Route path='/' element={<Layout/>}>
-    <Route path="login" element={<Login/>}/>
-    <Route path="register" element={<Register/>}/>
-    <Route path="/dashboard" element={<Dashboard/>} />
-    <Route path="/" element={<Login />} /> {/* Default route to login */}
-    </Route>
-   </Routes>
-   
-   </HashRouter > 
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<h2>Welcome to the RBAC System</h2>} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="dashboard" element={<Dashboard />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
